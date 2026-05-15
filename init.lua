d302debba25a68046ffc2da6d51f1228ff4c6f78
Bram Aristyo <bram.mymen@gmail.com>
2026-05-15 15:05:20 +0700

    minimal init.lua for golang and py LSP

--- init.lua
+++ init.lua
@@ -1,4 +1,67 @@
-vim.wo.number = true
-vim.opt.relativenumber = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.g.mapleader        = " "
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.tabstop        = 4
+vim.opt.shiftwidth     = 4
+vim.opt.expandtab      = true
+vim.opt.clipboard      = "unnamedplus" -- use system clipboard
+
+local map = vim.keymap.set
+map("i", "jk", "<Esc>")                       
+map("n", "<leader>w", ":w<CR>")               
+map("n", "<leader>q", ":q<CR>")               
+map("n", "<C-h>", "<C-w>h")                   
+map("n", "<C-l>", "<C-w>l")                   
+map("n", "<C-j>", "<C-w>j")                   
+map("n", "<C-k>", "<C-w>k")                   
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+  { "williamboman/mason.nvim",            config = true },
+  { "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "gopls", "pyright" }, 
+        automatic_installation = true,
+      })
+    end },
+
+  { "neovim/nvim-lspconfig",
+    config = function()
+      -- new API: vim.lsp.config (nvim 0.11+)
+      vim.lsp.config("gopls",  {})
+      vim.lsp.config("pyright", {})
+      vim.lsp.enable({ "gopls", "pyright" })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(e)
+          local o = { buffer = e.buf }
+          map("n", "gd",         vim.lsp.buf.definition, o) 
+          map("n", "K",          vim.lsp.buf.hover,      o) 
+          map("n", "<leader>rn", vim.lsp.buf.rename,     o) 
+        end,
+      })
+    end },
+
+  { "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer" },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"]   = cmp.mapping.select_next_item(), -- next suggestion
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- prev suggestion
+          ["<CR>"]    = cmp.mapping.confirm({ select = true }), -- confirm
+        }),
+        sources = { { name = "nvim_lsp" }, { name = "buffer" } },
+      })
+    end },
+
+})
